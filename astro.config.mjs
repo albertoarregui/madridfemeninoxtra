c799fc826e225e717399abd09039c095a08d755a
SvavaPrime <aarreguijimenez@gmail.com>
2025-12-05 09:18:20 +0100

    Descripción de cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
+
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-
   vite: {
     plugins: [tailwindcss()]
   },
