1d3fdd56829e747dbbaadaf97b929378da2b16be
SvavaPrime <aarreguijimenez@gmail.com>
2025-12-05 09:13:50 +0100

    Descripción de cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  output: 'hybrid', // Habilita SSR para rutas API dinámicas
+
   vite: {
     plugins: [tailwindcss()]
   },
