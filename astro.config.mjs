e158eeecc6bf2a7b2c319f197b4c8d315a8ffad6
SvavaPrime <aarreguijimenez@gmail.com>
2025-12-23 20:45:38 +0100

    Descripción de cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://www.madridfemeninoxtra.com',
   output: 'server',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
