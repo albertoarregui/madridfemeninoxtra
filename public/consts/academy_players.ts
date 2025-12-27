b82dc261c4d90e7c3898702358219ed63cd4787e
SvavaPrime <aarreguijimenez@gmail.com>
2025-12-27 01:49:44 +0100

    feat: Update Awards page (larger cards, academy data, scroll nav, new icons)

---
+++ public/consts/academy_players.ts
@@ -0,0 +1,44 @@
+export const SQUAD = [
+    {
+        id: "noe-bejarano",
+        name: "Noe Bejarano",
+        position: "Lateral derecha",
+        date_birth: "2006-06-27",
+        country: "es",
+    },
+    {
+        id: "silvia-cristóbal",
+        name: "Silvia Cristóbal",
+        position: "Lateral derecha",
+        date_birth: "2008-05-01",
+        country: "es",
+    },
+    {
+        id: "adriana-folgado",
+        name: "Adriana Folgado",
+        position: "Centrocampista",
+        date_birth: "2007-04-01",
+        country: "es",
+    },
+    {
+        id: "irune-dorado",
+        name: "Irune Dorado",
+        position: "Centrocampista",
+        date_birth: "2008-03-22",
+        country: "es",
+    },
+    {
+        id: "naiara-sanmartín",
+        name: "Naiara Sanmartín",
+        position: "Extremo",
+        date_birth: "2006-01-23",
+        country: "es",
+    },
+    {
+        id: "iris-ashley-santiago",
+        name: "Iris Ashley",
+        position: "Delantera",
+        date_birth: "2007-11-09",
+        country: "es",
+    },
+]
